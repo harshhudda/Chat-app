7e79f36c100ddcc5c1b7806d8cadf414885cdc1d
harshhudda <harshu886025@gmail.com>
2025-06-04 00:20:37 +0530

    Final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -32,7 +31,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
